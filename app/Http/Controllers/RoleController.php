9956815cf781046e2e9ae8d7dce427bf0af40e27
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-14 13:59:10 +0530

    small updates

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -12,12 +12,12 @@ class RoleController extends Controller
     /**
      * Display a listing of the resource.
      */
-    /*function __construct(){
+    function __construct(){
          $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
          $this->middleware('permission:role-create', ['only' => ['create','store']]);
          $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
          $this->middleware('permission:role-delete', ['only' => ['destroy']]);
-    }*/
+    }
 
     public function index()
     {
