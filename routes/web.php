7c9b152b21d8895d232efa6da8877c6595ea91a6
Vijoy Sasidharan <devuinfosys@gmail.com>
2023-05-14 13:56:25 +0530

    small updates

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,11 @@ Route::get('/', function () {
 
 Route::post('/', [UserController::class, 'login'])->name('login');
 Route::get('/logout', [UserController::class, 'logout'])->name('logout');
-Route::post('store_branch_session', [UserController::class, 'store_branch_session'])->name('store_branch_session');
-Route::get('/dash', [UserController::class, 'dash'])->name('dash');
 
+Route::group(['middleware' => ['web', 'auth']], function(){  
+    Route::post('store_branch_session', [UserController::class, 'store_branch_session'])->name('store_branch_session');
+    Route::get('/dash', [UserController::class, 'dash'])->name('dash');
+});
 Route::group(['middleware' => ['web', 'auth', 'branch']], function(){   
 
     Route::get('/helper/createddlcat', [HelperController::class, 'createddlcat'])->name('createddlcat');
